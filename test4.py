683ebd71061b5ed30d1d601966b01683cb98047e
MinButMax <czds68@163.com>
2018-08-26 21:45:26 +0800

    test mac

--- test4.py
+++ test4.py
@@ -1,6 +1,10 @@
-import  os
+import os
+import re
 tt = os.system("sudo spoof-mac.py list")
 
 def GetMacAddress():
-    output = os.popen('sudo spoof-mac.py list')
-    print( output.read())
+    MacFilter = re.compile(r'[:0-9A-F]')
+    output = str(os.popen('sudo spoof-mac.py list'))
+    return ( MacFilter.findall(output)[0])
+
+print(GetMacAddress())
